e60864079d44a67e1d0529dc9d6cfeaa575f1baa
jeet mulani <103180016+jeetmulani@users.noreply.github.com>
2023-08-22 17:01:24 +0530

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,12 +5,4 @@ import * as validation from '../validation';
 const router = express.Router();
 
 
-router.post('/signUp', validation?.signUp, authController.signUp)
-router.post('/login', validation?.login, authController.login)
-router.post('/forgot_password', validation.forgot_password, authController.forgot_password)
-router.post('/resend_otp', validation.resend_otp, authController.resend_otp)
-router.post('/otp_verification', validation?.otp_verification, authController.otp_verification)
-router.post('/reset_password', validation.reset_password, authController.reset_password)
-
-
-export const authRouter = router;   
+router.post('/signUp', validation?.signUp, authController.signU
